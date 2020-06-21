43e639121eba213656a3449b56b1ad50b16e055c
Ryan Talbot <RyanTalbot7@gmail.com>
2020-06-21 18:28:04 -0500

    Fix bugs in app.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
-fclass App extends Component {
+class App extends Component {
   render() {
-      return (
-          <div className="App">
-              <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">NuCamp</NavbarBrand>
-              </div>
-              </Navbar>
+    return (
+      <div className='App'>
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='/'>NuCamp</NavbarBrand>
           </div>
-      );
+        </Navbar>
+      </div>
+    );
   }
 }
 
