341970882b61bb88493e86824538226dc5cb8c4b
Ryan Talbot <RyanTalbot7@gmail.com>
2020-06-21 20:57:59 -0500

    Add react components and personalize

---
+++ src/shared/campsites.js
@@ -0,0 +1,184 @@
+export const CAMPSITES = [
+  {
+    id: 0,
+    name: 'Lausanne, Switzerland',
+    image: 'assets/images/lausanne.jpg',
+    elevation: 1726,
+    description:
+      'Lausanne is the capital city and biggest town of the canton of Vaud in Romandy, Switzerland',
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        text: 'What a magnificent view!',
+        author: 'Ryan Talbot',
+        date: '2019-12-25T16:30Z'
+      },
+      {
+        id: 1,
+        rating: 4,
+        text:
+          'The campground was beautiful, but the bugs could bite sometimes.',
+        author: 'Brennen Ech',
+        date: '2017-06-17T03:33Z'
+      },
+      {
+        id: 2,
+        rating: 5,
+        text: 'I caught a world-record-breaking trout here on my last visit.',
+        author: 'McKenzie Sebastian',
+        date: '2019-02-18T18:12Z'
+      },
+      {
+        id: 3,
+        rating: 5,
+        text: 'A wonderful place to reconnect with nature.',
+        author: 'Jordan Runn',
+        date: '2019-08-04T20:11Z'
+      },
+      {
+        id: 4,
+        rating: 4,
+        text: 'The stars at night were magnificent!',
+        author: 'Ann Dabramov',
+        date: '2018-07-23T19:44Z'
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: 'Hamburg, Germany',
+    image: 'assets/images/hamburg.jpg',
+    elevation: 381,
+    description:
+      'Hamburg officially the Free and Hanseatic City of Hamburg (German: Freie und Hansestadt Hamburg; Low Saxon: Friee un Hansestadt Hamborg), is the second-largest city in Germany after Berlin and 7th largest city in the European Union with a population of over 1.84 million',
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        text: 'What a magnificent view!',
+        author: 'Tinus Lorvaldes',
+        date: '2018-10-25T16:30Z'
+      },
+      {
+        id: 1,
+        rating: 4,
+        text:
+          'The campground was beautiful, but the bugs could bite sometimes.',
+        author: 'Brennen Ech',
+        date: '2017-06-17T03:33Z'
+      },
+      {
+        id: 2,
+        rating: 5,
+        text: 'I caught a world-record-breaking trout here on my last visit.',
+        author: 'McKenzie Sebastian',
+        date: '2019-02-18T18:12Z'
+      },
+      {
+        id: 3,
+        rating: 5,
+        text: 'A wonderful place to reconnect with nature.',
+        author: 'Jordan Runn',
+        date: '2019-08-04T20:11Z'
+      },
+      {
+        id: 4,
+        rating: 4,
+        text: 'The stars at night were magnificent!',
+        author: 'Ann Dabramov',
+        date: '2018-07-23T19:44Z'
+      }
+    ]
+  },
+  {
+    id: 2,
+    name: 'Berlin, Germany',
+    image: 'assets/images/berlin.jpg',
+    elevation: 2901,
+    description: 'Berlin is the capital and largest city of Germany by both area and population. Its 3,769,495 inhabitants make it the most populous city proper of the European Union.'[
+      ({
+        id: 0,
+        rating: 5,
+        text: 'What a magnificent view!',
+        author: 'Tinus Lorvaldes',
+        date: '2018-10-25T16:30Z'
+      },
+      {
+        id: 1,
+        rating: 4,
+        text:
+          'The campground was beautiful, but the bugs could bite sometimes.',
+        author: 'Brennen Ech',
+        date: '2017-06-17T03:33Z'
+      },
+      {
+        id: 2,
+        rating: 5,
+        text: 'I caught a world-record-breaking trout here on my last visit.',
+        author: 'McKenzie Sebastian',
+        date: '2019-02-18T18:12Z'
+      },
+      {
+        id: 3,
+        rating: 5,
+        text: 'A wonderful place to reconnect with nature.',
+        author: 'Jordan Runn',
+        date: '2019-08-04T20:11Z'
+      },
+      {
+        id: 4,
+        rating: 4,
+        text: 'The stars at night were magnificent!',
+        author: 'Ann Dabramov',
+        date: '2018-07-23T19:44Z'
+      })
+    ]
+  },
+  {
+    id: 3,
+    name: 'New Orleans',
+    image: 'assets/images/nola.jpg',
+    elevation: 42,
+    description:
+      'New Orleans is a consolidated city-parish located along the Mississippi River in the southeastern region of the U.S. state of Louisiana. With an estimated population of 390,144 in 2019,[6] it is the most populous city in Louisiana. Serving as a major port, New Orleans is considered an economic and commercial hub for the broader Gulf Coast region of the United States.',
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        text: 'What a magnificent view!',
+        author: 'Tinus Lorvaldes',
+        date: '2018-10-25T16:30Z'
+      },
+      {
+        id: 1,
+        rating: 4,
+        text:
+          'The campground was beautiful, but the bugs could bite sometimes.',
+        author: 'Brennen Ech',
+        date: '2017-06-17T03:33Z'
+      },
+      {
+        id: 2,
+        rating: 5,
+        text: 'I caught a world-record-breaking trout here on my last visit.',
+        author: 'McKenzie Sebastian',
+        date: '2019-02-18T18:12Z'
+      },
+      {
+        id: 3,
+        rating: 5,
+        text: 'A wonderful place to reconnect with nature.',
+        author: 'Jordan Runn',
+        date: '2019-08-04T20:11Z'
+      },
+      {
+        id: 4,
+        rating: 4,
+        text: 'The stars at night were magnificent!',
+        author: 'Ann Dabramov',
+        date: '2018-07-23T19:44Z'
+      }
+    ]
+  }
+];
